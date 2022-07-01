d61e7a28e8c0c665a75e4eb74e3410edfaedacf6
Aldair Raul Cosetito Coral <aldair.loq.369@gmail.com>
2022-06-30 22:56:30 -0500

    Initial Commit

---
+++ TMDB-SwiftUI/TMDB_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TMDB_SwiftUIApp.swift
+//  TMDB-SwiftUI
+//
+//  Created by Aldair Cosetito Coral on 30/06/22.
+//
+
+import SwiftUI
+
+@main
+struct TMDB_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
