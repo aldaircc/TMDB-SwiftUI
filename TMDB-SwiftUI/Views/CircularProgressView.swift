479dcabb12286c5fc5f0baf89ff7a4c79f8ac96b
Aldair Raul Cosetito Coral <aldair.loq.369@gmail.com>
2022-06-30 22:57:44 -0500

    base files for circular progress view.

---
+++ TMDB-SwiftUI/Views/CircularProgressView.swift
@@ -0,0 +1,20 @@
+//
+//  CircularProgressView.swift
+//  TMDB-SwiftUI
+//
+//  Created by Aldair Cosetito Coral on 30/06/22.
+//
+
+import SwiftUI
+
+struct CircularProgressView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct CircularProgressView_Previews: PreviewProvider {
+    static var previews: some View {
+        CircularProgressView()
+    }
+}
