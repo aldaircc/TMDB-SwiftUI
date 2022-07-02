81633b4d9f4ba4e20896c245a7ee8639178c0cf1
Aldair Raul Cosetito Coral <aldair.loq.369@gmail.com>
2022-07-02 17:18:12 -0500

    Add the missing contentView.

---
+++ TMDB-SwiftUI/Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  TMDB-SwiftUI
+//
+//  Created by Aldair Cosetito Coral on 2/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
