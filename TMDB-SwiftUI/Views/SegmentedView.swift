5e09e4484aa6d5fa85f1721eb7ef48bdd5e30ab1
Aldair Raul Cosetito Coral <aldair.loq.369@gmail.com>
2022-07-02 00:00:52 -0500

    base file to create segmented view.

---
+++ TMDB-SwiftUI/Views/SegmentedView.swift
@@ -0,0 +1,20 @@
+//
+//  SegmentedView.swift
+//  TMDB-SwiftUI
+//
+//  Created by Aldair Cosetito Coral on 1/07/22.
+//
+
+import SwiftUI
+
+struct SegmentedView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct SegmentedView_Previews: PreviewProvider {
+    static var previews: some View {
+        SegmentedView()
+    }
+}
