66f923b0d4a02c9cf7f5a1e6246bed221f0fdff6
Aldair Raul Cosetito Coral <aldair.loq.369@gmail.com>
2022-07-02 22:19:22 -0500

    base files

---
+++ TMDB-SwiftUI/Views/MovieCardView.swift
@@ -0,0 +1,20 @@
+//
+//  MovieCardView.swift
+//  TMDB-SwiftUI
+//
+//  Created by Aldair Cosetito Coral on 2/07/22.
+//
+
+import SwiftUI
+
+struct MovieCardView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct MovieCardView_Previews: PreviewProvider {
+    static var previews: some View {
+        MovieCardView()
+    }
+}
