7b8ecc9f9b4dfc4c30c1c757ad950cf09aafb249
Aldair Raul Cosetito Coral <aldair.loq.369@gmail.com>
2022-07-03 20:08:08 -0500

    Custom segmented control base file.

---
+++ TMDB-SwiftUI/Views/CustomSegmentedControl.swift
@@ -0,0 +1,20 @@
+//
+//  CustomSegmentedControl.swift
+//  TMDB-SwiftUI
+//
+//  Created by Aldair Cosetito Coral on 3/07/22.
+//
+
+import SwiftUI
+
+struct CustomSegmentedControl: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CustomSegmentedControl_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomSegmentedControl()
+    }
+}
