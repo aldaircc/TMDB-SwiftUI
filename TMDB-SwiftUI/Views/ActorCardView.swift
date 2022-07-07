5939630636756fc49ccbef07f266bbee4a150f26
Aldair Raul Cosetito Coral <aldair.loq.369@gmail.com>
2022-07-06 22:30:44 -0500

    border and shadow applied to ZStack.

--- TMDB-SwiftUI/Views/ActorCardView.swift
+++ TMDB-SwiftUI/Views/ActorCardView.swift
@@ -9,15 +9,38 @@ import SwiftUI
 
 struct ActorCardView: View {
     var body: some View {
-        VStack(alignment: .leading, spacing: 10) {
+        ZStack(alignment: .bottomLeading) {
             Image("Gillian Jacobs")
                 .resizable()
-                .frame(width: 120, height: 175, alignment: .center)
-
+                .scaledToFit()
+                .frame(width: 117, height: 175, alignment: .center)
+                .background(.yellow)
+                .cornerRadius(10)
+//                .overlay(
+//                    VStack(alignment: .leading) {
+//                        Spacer()
+//                        HStack {
+//                            Text("Gillian Jacobs")
+//                                .font(.system(size: 14))
+//                                .fontWeight(.semibold)
+//                            Spacer()
+//                        }
+//                        .background(Color.white)
+//                        HStack {
+//                            Text("Brianne")
+//                                .font(.system(size: 11))
+//                                .fontWeight(.light)
+//                            Spacer()
+//                        }
+//                        .background(Color.white)
+//                    }
+//                )
         }
-        .background(.red)
-        .cornerRadius(10)
-        .padding()
+        .overlay(
+            RoundedRectangle(cornerRadius: 9, style: .circular)
+                .stroke(.white, lineWidth: 1)
+                .shadow(color: .black, radius: 3, x: 2, y: 0)
+        )
     }
 }
 
