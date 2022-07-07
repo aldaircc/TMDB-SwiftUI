eebd145b9809cbdaeb9e6c945053aa72e9330092
Aldair Raul Cosetito Coral <aldair.loq.369@gmail.com>
2022-07-06 19:35:54 -0500

    Base file.

---
+++ TMDB-SwiftUI/Views/ActorCardView.swift
@@ -0,0 +1,20 @@
+//
+//  ActorCardView.swift
+//  TMDB-SwiftUI
+//
+//  Created by Aldair Cosetito Coral on 6/07/22.
+//
+
+import SwiftUI
+
+struct ActorCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ActorCardView_Previews: PreviewProvider {
+    static var previews: some View {
+        ActorCardView()
+    }
+}
