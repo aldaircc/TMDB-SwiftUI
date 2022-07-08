83ffde2f5d5bf82d7991e55648cc9c633ab6f107
Aldair Raul Cosetito Coral <aldair.loq.369@gmail.com>
2022-07-07 20:15:12 -0500

    base file created.

---
+++ TMDB-SwiftUI/Views/StoryViewerView.swift
@@ -0,0 +1,20 @@
+//
+//  StoryViewerView.swift
+//  TMDB-SwiftUI
+//
+//  Created by Aldair Cosetito Coral on 7/07/22.
+//
+
+import SwiftUI
+
+struct StoryViewerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct StoryViewerView_Previews: PreviewProvider {
+    static var previews: some View {
+        StoryViewerView()
+    }
+}
