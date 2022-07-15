50a2bc7b5340ab52ebe0f9fff980672e5906e29f
Aldair Raul Cosetito Coral <aldair.loq.369@gmail.com>
2022-07-14 19:17:19 -0500

    movie model defined

---
+++ TMDB-SwiftUI/Model/Enums.swift
@@ -0,0 +1,61 @@
+//
+//  Enums.swift
+//  TMDB-SwiftUI
+//
+//  Created by Aldair Cosetito Coral on 13/07/22.
+//
+
+import Foundation
+
+enum CustomError: Error {
+    case httpResponse(Int)
+    case jsonDecoder(Error)
+}
+
+enum MediaType: String, Codable {
+    case all
+    case movie
+    case tv
+    case person
+}
+
+enum KnownForDepartment: String, Codable {
+    case acting = "Acting"
+    case directing = "Directing"
+}
+
+enum OriginalLanguage: String, Codable {
+    case en = "en"
+    case ja = "ja"
+    case fr = "fr"
+    case de = "de"
+    case ta = "ta"
+    case ko = "ko"
+}
+
+enum Languages: CustomStringConvertible {
+    case english
+    case french
+    case german
+    case japanese
+    case spanish
+    case chinese
+    
+    var description: String {
+        switch self {
+        case .english:
+            return "en"
+        case .french:
+            return "fr"
+        case .german:
+            return "de"
+        case .japanese:
+            return "ja"
+        case .spanish:
+            return "es"
+        case .chinese:
+            return "zh"
+        }
+    }
+}
+
