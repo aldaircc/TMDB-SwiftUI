d1466a75542fbadb30824a22914588ba2778b7c8
Aldair Raul Cosetito Coral <aldair.loq.369@gmail.com>
2022-07-15 20:51:15 -0500

    Image model implemented

--- TMDB-SwiftUI/Model/ImageModel.swift
+++ TMDB-SwiftUI/Model/ImageModel.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+
+struct ImageModel: Codable {
+    let aspectRatio: Double
+    let height: Int
+    let iso6391: String?
+    let filePath: String
+    let voteAverage: Double
+    let voteCount: Int
+    let width: Int
+    
+    enum CodingKeys: String, CodingKey {
+    case aspectRatio = "aspect_ratio"
+        case height, width
+        case iso6391 = "iso_639_1"
+        case filePath = "file_path"
+        case voteAverage = "vote_average"
+        case voteCount = "vote_count"
+    }
+}
+
+struct ImageResult: Codable {
+    let backDrops: [ImageModel]
+    
+    enum CodingKeys: String, CodingKey {
+        case backDrops = "backdrops"
+    }
+}
