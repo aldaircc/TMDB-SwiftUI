ff1df196387c1b9ee73e75574a13fd48ec4fdca7
Aldair Raul Cosetito Coral <aldair.loq.369@gmail.com>
2022-07-17 14:57:41 -0500

    Rate movie function created.

--- TMDB-SwiftUI/Network/Network.swift
+++ TMDB-SwiftUI/Network/Network.swift
@@ -133,4 +133,27 @@ struct Network {
             return nil
         }
     }
+    
+    /// Rating
+    func rateMovie(_ movieId: Int) async throws -> ResponseModel? {
+        let components = URLComponents(string: "\(URL.movie)\(movieId)/rating")
+        guard let url = components?.url?.addApiKey() else {
+            return nil
+        }
+        
+        do {
+            let (data, response) = try await URLSession.shared.data(from: url)
+            guard let httpResponse = response as? HTTPURLResponse  else {
+                return nil
+            }
+            if httpResponse.statusCode == 200 {
+                let result = try JSONDecoder().decode(ResponseModel.self, from: data)
+                return result
+            } else {
+                return nil
+            }
+        } catch {
+            return nil
+        }
+    }
 }
