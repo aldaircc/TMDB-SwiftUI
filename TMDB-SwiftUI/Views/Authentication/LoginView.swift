54f5b7343f3a3e04157a96fc9aa33cd518efdd3c
Aldair Raul Cosetito Coral <aldair.loq.369@gmail.com>
2022-07-25 20:26:42 -0500

    New color added and set font weight for primary button style.

--- TMDB-SwiftUI/Views/Authentication/LoginView.swift
+++ TMDB-SwiftUI/Views/Authentication/LoginView.swift
@@ -11,12 +11,13 @@ struct RoundedButtonStyle: ButtonStyle {
     func makeBody(configuration: Configuration) -> some View {
         HStack {
             configuration.label
+                .font(.system(size: 17, weight: .bold))
                 .foregroundColor(.white)
         }
         .frame(maxWidth: .infinity, idealHeight: 16)
         .padding(5)
         .background(
-            configuration.isPressed ? Color.blue : Color.teal
+            configuration.isPressed ? Color("Button background Pressed") : Color("Button background No Pressed")
         )
         .cornerRadius(5)
         .overlay(
