e663294277f5a93be7c7362643eb7c1abb74a906
Aldair Raul Cosetito Coral <aldair.loq.369@gmail.com>
2022-07-26 19:44:12 -0500

    RoundedTextFieldStyle file created.

--- TMDB-SwiftUI/Views/Authentication/LoginView.swift
+++ TMDB-SwiftUI/Views/Authentication/LoginView.swift
@@ -7,22 +7,6 @@
 
 import SwiftUI
 
-struct RoundedTextFieldStyle: TextFieldStyle {
-    var isEditing: Bool
-    
-    func _body(configuration: TextField<Self._Label>) -> some View {
-        return configuration
-            .padding(
-                EdgeInsets(top: 10, leading: 5, bottom: 10, trailing: 5)
-            )
-            .overlay(
-                RoundedRectangle(cornerRadius: 5)
-                    .stroke(lineWidth: 1)
-                    .fill(Color(isEditing ? "TextField Border Color On Focus" : "TextField Border Color Lost Focus"))
-            )
-    }
-}
-
 struct RoundedButtonStyle: ButtonStyle {
     func makeBody(configuration: Configuration) -> some View {
         HStack {
