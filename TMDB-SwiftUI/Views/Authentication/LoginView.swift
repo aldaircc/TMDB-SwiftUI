04337cccc4f7835c89f73df5316f72c98f92d9b2
Aldair Raul Cosetito Coral <aldair.loq.369@gmail.com>
2022-07-26 19:45:08 -0500

    RoundedButtonStyle defined.

--- TMDB-SwiftUI/Views/Authentication/LoginView.swift
+++ TMDB-SwiftUI/Views/Authentication/LoginView.swift
@@ -7,27 +7,6 @@
 
 import SwiftUI
 
-struct RoundedButtonStyle: ButtonStyle {
-    func makeBody(configuration: Configuration) -> some View {
-        HStack {
-            configuration.label
-                .font(.system(size: 17, weight: .bold))
-                .foregroundColor(.white)
-        }
-        .frame(maxWidth: .infinity, idealHeight: 16)
-        .padding(5)
-        .background(
-            configuration.isPressed ? Color("Button background Pressed") : Color("Button background No Pressed")
-        )
-        .cornerRadius(5)
-        .overlay(
-            RoundedRectangle(cornerRadius: 5)
-                .stroke(lineWidth: 1)
-                .fill(Color.gray.opacity(0.3))
-        )
-    }
-}
-
 struct LoginView: View {
     @State var isEditing: Bool = false
     var body: some View {
