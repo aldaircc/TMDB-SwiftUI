38791fcb87ad0939a946464a67aa695f70d2ac26
Aldair Raul Cosetito Coral <aldair.loq.369@gmail.com>
2022-09-06 18:32:40 -0500

    Methods defined to save and validate tokenkey time

--- TMDB-SwiftUI/Shared/SessionManagement.swift
+++ TMDB-SwiftUI/Shared/SessionManagement.swift
@@ -8,5 +8,26 @@
 import Foundation
 
 struct SessionManagement {
+    static let shared = SessionManagement()
     
+    var apiKey: String?
+    var sessionId: String?
+    var expiresAt: String?
+    
+    
+    func validationSession() -> Bool {
+        return true
+    }
+    
+    mutating func saveApiKey(_ value: String) {
+        self.apiKey = value
+    }
+    
+    mutating func saveSession(_ value: String) {
+        self.sessionId = value
+    }
+    
+    mutating func saveExpiresAt(_ value: String) {
+        self.expiresAt = value
+    }
 }
