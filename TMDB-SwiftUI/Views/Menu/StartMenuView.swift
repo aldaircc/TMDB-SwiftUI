32204bf20405fc6f84734eb24701286fb204f54b
Aldair Raul Cosetito Coral <aldair.loq.369@gmail.com>
2022-09-09 21:47:24 -0500

    Added base files to create start menu

---
+++ TMDB-SwiftUI/Views/Menu/StartMenuView.swift
@@ -0,0 +1,20 @@
+//
+//  StartMenuView.swift
+//  TMDB-SwiftUI
+//
+//  Created by Aldair Cosetito Coral on 9/09/22.
+//
+
+import SwiftUI
+
+struct StartMenuView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct StartMenuView_Previews: PreviewProvider {
+    static var previews: some View {
+        StartMenuView()
+    }
+}
