84346e3c3a0777b1002b1c593341c0f7cd4262a7
Aldair Raul Cosetito Coral <aldair.loq.369@gmail.com>
2022-09-10 23:01:02 -0500

    Home view added to show trend movies

---
+++ TMDB-SwiftUI/Views/Menu/HomeView.swift
@@ -0,0 +1,28 @@
+//
+//  HomeView.swift
+//  TMDB-SwiftUI
+//
+//  Created by Aldair Cosetito Coral on 10/09/22.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Hello, World!")
+                Spacer()
+                Button("See all") {
+                    
+                }
+            }
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
