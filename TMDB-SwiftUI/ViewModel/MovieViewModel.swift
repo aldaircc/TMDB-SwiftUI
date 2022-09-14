01f73a4acff812682a4a1815f978fa22d04c3243
Aldair Raul Cosetito Coral <aldair.loq.369@gmail.com>
2022-09-13 19:04:08 -0500

    Made object heritages Identifiable protocol

--- TMDB-SwiftUI/ViewModel/MovieViewModel.swift
+++ TMDB-SwiftUI/ViewModel/MovieViewModel.swift
@@ -10,6 +10,11 @@ import Foundation
 final class MovieViewModel: ObservableObject {
     
     let network: Network
+    var trendMovies: [ResultTrending] {
+        return trendingResult?.results ?? []
+    } //TrendingResult = []
+    
+    @Published var trendingResult: TrendingResult?
     
     init(_ network: Network = Network()) {
         self.network = network
@@ -17,6 +22,7 @@ final class MovieViewModel: ObservableObject {
     
     func getTrendingMovies(_ page: Int = 1, mediaType: String = "person", timeWindow: String = "week") async -> TrendingResult? {
         let movies = await network.getTrendings(page, mediaType: mediaType, timeWindow: timeWindow)
+        trendingResult = movies
         return movies
     }
 }
