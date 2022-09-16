302d47be61755e06b1262424c4efc74f75d4e432
Aldair Raul Cosetito Coral <aldair.loq.369@gmail.com>
2022-09-15 20:05:59 -0500

    Base file to create card added

---
+++ TMDB-SwiftUI/Views/Shared/CardView.swift
@@ -0,0 +1,20 @@
+//
+//  CardView.swift
+//  TMDB-SwiftUI
+//
+//  Created by Aldair Cosetito Coral on 15/09/22.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CardView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardView()
+    }
+}
