697fb5be4cca0ea71aae68bd64c8a8f90b101c0c
Aldair Raul Cosetito Coral <aldair.loq.369@gmail.com>
2022-09-18 20:47:58 -0500

    Added container for bottom section including circle

--- TMDB-SwiftUI/Views/Shared/CardView.swift
+++ TMDB-SwiftUI/Views/Shared/CardView.swift
@@ -9,27 +9,36 @@ import SwiftUI
 
 struct CardView: View {
     var body: some View {
-        ZStack(alignment: .bottomLeading) {
-            Image("greysAnatomy")
-                .resizable()
-                .frame(width: 120, height: 185)
-                .cornerRadius(9)
-            
-            ZStack {
-                Circle()
-                Circle()
-                    .trim(from: 0.0, to: 0.5)
-                    .stroke(style: StrokeStyle(lineWidth: 1, lineCap: .round, lineJoin: .round))
-                    .rotationEffect(Angle.degrees(-90))
-                    .foregroundColor(.orange)
+        GeometryReader { proxy in
+            ZStack(alignment: .bottomLeading) {
+                Image("greysAnatomy")
+                    .resizable()
+                    .frame(width: 120, height: 185)
+                    .cornerRadius(9)
+                
+                VStack(alignment: .leading, spacing: 10) {
+                    ZStack {
+                        Circle()
+                        Circle()
+                            .trim(from: 0.0, to: 0.5)
+                            .stroke(style: StrokeStyle(lineWidth: 1, lineCap: .round, lineJoin: .round))
+                            .rotationEffect(Angle.degrees(-90))
+                            .foregroundColor(.orange)
+                    }
+                    .frame(width: 30)
+                    
+                    VStack {
+                        Text("My favorite movie")
+                        Text("Release date")
+                            .font(.system(size: 11, weight: .thin))
+                            .foregroundColor(.gray.opacity(0.6))
+                    }
+                }
+                .background(.red)
             }
-            .frame(width: 30)
-            
-            Text("My favorite movie")
-            Text("Release date")
-                .font(.system(size: 11, weight: .thin))
-                .foregroundColor(.gray.opacity(0.6))
         }
+        .background(.orange)
+        .frame(width: 120, height: 185)
     }
 }
 
