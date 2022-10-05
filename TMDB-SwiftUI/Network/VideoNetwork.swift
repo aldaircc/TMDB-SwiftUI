d7dac50bb36ac912d3adafb1b59a817242eee998
Aldair Raul Cosetito Coral <aldair.loq.369@gmail.com>
2022-10-04 20:14:59 -0500

    Consumed api oK

--- TMDB-SwiftUI/Network/VideoNetwork.swift
+++ TMDB-SwiftUI/Network/VideoNetwork.swift
@@ -13,6 +13,29 @@ protocol VideoRequestProtocol {
 
 struct VideoNetwork: VideoRequestProtocol {
     func getVideo(movieId: Int) {
+        let components = URLComponents(string: URL.movieVideo.absoluteString.replacingOccurrences(of: "movie_id", with: String(movieId)))
         
+        guard let url = components?.url?.addApiKey() else {
+            return
+        }
+        
+        var request = URLRequest(url: url)
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "Get"
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data, let httpResponse = response as? HTTPURLResponse else {
+                print(error?.localizedDescription)
+                return
+            }
+            
+            if httpResponse.statusCode >= 200 && httpResponse.statusCode < 300 {
+                print("Good job", data)
+            } else {
+                print("There was an error, statusCode: \(httpResponse.statusCode)")
+            }
+            
+            return
+        }.resume()
     }
 }
