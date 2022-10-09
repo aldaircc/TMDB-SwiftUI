abafd8b0beb97f4586cdd1703a5cbfd8686e555c
Aldair Raul Cosetito Coral <aldair.loq.369@gmail.com>
2022-10-08 23:30:02 -0500

    Added base file to added detail view

---
+++ TMDB-SwiftUI/Views/Detail/MovieDetailView.swift
@@ -0,0 +1,25 @@
+//
+//  MovieDetailView.swift
+//  TMDB-SwiftUI
+//
+//  Created by Aldair Cosetito Coral on 7/10/22.
+//
+
+import SwiftUI
+
+struct MovieDetailView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, World!")
+            Section {
+                Text("Hello, World!")
+            }
+        }
+    }
+}
+
+struct MovieDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        MovieDetailView()
+    }
+}
