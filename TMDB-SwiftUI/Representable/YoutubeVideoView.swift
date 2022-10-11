50ee6c1c202556be7310684aa2e7c61facd4b4c9
Aldair Raul Cosetito Coral <aldair.loq.369@gmail.com>
2022-10-10 19:19:31 -0500

    Added content for methods makeUIView for start state and updateView to make sure the changes are going to applied when send signals from swiftui

--- TMDB-SwiftUI/Representable/YoutubeVideoView.swift
+++ TMDB-SwiftUI/Representable/YoutubeVideoView.swift
@@ -13,12 +13,20 @@ struct YoutubeVideoView: UIViewRepresentable {
     
     typealias UIViewType = WKWebView
     
+    var videoIdentifier: String
+    
     func makeUIView(context: Context) -> WKWebView {
         WKWebView()
     }
     
     func updateUIView(_ uiView: WKWebView, context: Context) {
+        let path = "https://www.youtube.com/watch?v=\(videoIdentifier)"
         
+        guard let url = URL(string: path) else {
+            return
+        }
+        uiView.scrollView.isScrollEnabled = false
+        uiView.load(URLRequest(url: url))
     }
     
     
