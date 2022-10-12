45fa9b6e58631fd1c174c4783f227cbe4a5f8b9e
Aldair Raul Cosetito Coral <aldair.loq.369@gmail.com>
2022-10-11 22:23:25 -0500

    Added functionality

---
+++ TMDB-SwiftUI/Views/Splash/SplashView.swift
@@ -0,0 +1,40 @@
+//
+//  SplashView.swift
+//  TMDB-SwiftUI
+//
+//  Created by Aldair Cosetito Coral on 11/10/22.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    @State var isActive = false
+    @State var opacity = 0.3
+    
+    var body: some View {
+        if isActive {
+            MovieDetailView()
+        } else {
+            ZStack {
+                Image("tmdbLogo")
+                    .opacity(opacity)
+                    .onAppear {
+                        withAnimation(.easeIn(duration: 0.5)) {
+                            self.opacity = 1
+                        }
+                    }
+            }
+            .onAppear {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                    self.isActive = true
+                }
+            }
+        }
+    }
+}
+
+struct SplashView_Previews: PreviewProvider {
+    static var previews: some View {
+        SplashView()
+    }
+}
