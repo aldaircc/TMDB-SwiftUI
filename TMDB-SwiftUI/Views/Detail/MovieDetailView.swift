7b32efc4257c3a071054e892f962da0ba20b6249
Aldair Raul Cosetito Coral <aldair.loq.369@gmail.com>
2022-10-17 22:44:44 -0500

    Applied multitextalignment modifier to text view

--- TMDB-SwiftUI/Views/Detail/MovieDetailView.swift
+++ TMDB-SwiftUI/Views/Detail/MovieDetailView.swift
@@ -12,80 +12,96 @@ struct MovieDetailView: View {
     
     var body: some View {
         NavigationView {
-            VStack(alignment: .leading) {
-                /// Image
-                Image("example_detail_pic")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                
-                
-                
-                /// Category - year - duration (hours and minutes)
-                /// details/description of movie
-                
+            ScrollView(.vertical) {
                 VStack(alignment: .leading) {
-                    Text("Overview")
-                        .fontWeight(.bold)
+                    /// Image
+                    Image("example_detail_pic")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
                     
-                    Text("A ticking-time-bomb insomniac and a slippery soap salesman channel primal male aggression into a shocking new form of therapy. Their concept catches on, with underground \"fight clubs\" forming in every town, until an eccentric gets in the way and ignites an out-of-control spiral toward oblivion.")
+                    /// Category - year - duration (hours and minutes)
+                    /// details/description of movie
                     
-                    Divider()
-                    
-                    HStack {
-                        VStack(alignment: .leading) {
-                            Text("Status")
-                                .fontWeight(.bold)
-                            Text("Released")
-                        }
-                        Spacer()
-                        VStack(alignment: .leading) {
-                            Text("Original Language")
-                                .fontWeight(.bold)
-                            Text("English")
-                        }
-                    }
-                }
-                .padding(.horizontal)
-                
-                
-                /// Series Cast
-                Section {
-                    HStack(alignment: .top) {
-                        Text("Series Cast")
+                    VStack(alignment: .leading) {
+                        Text("Overview")
                             .fontWeight(.bold)
-                        Spacer()
-                    }
-                    
-                }
-                
-                /// Score Breakdown
-                ///
-                Section {
-                    HStack {
-                        Text("Media")
-                            .fontWeight(.bold)
-                        Spacer()
-                    }
-                    
-                    
-                    
-                    ScrollView(.horizontal) {
-                        LazyHStack(alignment: .top) {
-                            
-                            
+                        
+                        Text("A ticking-time-bomb insomniac and a slippery soap salesman channel primal male aggression into a shocking new form of therapy. Their concept catches on, with underground \"fight clubs\" forming in every town, until an eccentric gets in the way and ignites an out-of-control spiral toward oblivion.")
+                            .multilineTextAlignment(.leading)
+                        
+                        Divider()
+                        
+                        HStack {
+                            VStack(alignment: .leading) {
+                                Text("Status")
+                                    .fontWeight(.bold)
+                                Text("Released")
+                            }
+                            Spacer()
+                            VStack(alignment: .leading) {
+                                Text("Original Language")
+                                    .fontWeight(.bold)
+                                Text("English")
+                            }
                         }
+                        
+                        HStack {
+                            VStack(alignment: .leading) {
+                                Text("Budget")
+                                    .fontWeight(.bold)
+                                Text("$2,80")
+                            }
+                            Spacer()
+                            VStack(alignment: .leading) {
+                                Text("Revenue")
+                                    .fontWeight(.bold)
+                                Text("$14,575")
+                            }
+                        }
+                        
                     }
-                    .frame(height: 200, alignment: .leading)
+                    .padding(.horizontal)
+                    
+                    
+                    /// Series Cast
+                    Section {
+                        HStack(alignment: .top) {
+                            Text("Series Cast")
+                                .fontWeight(.bold)
+                            Spacer()
+                        }
+                        
+                    }
+                    
+                    /// Score Breakdown
+                    ///
+                    Section {
+                        HStack {
+                            Text("Media")
+                                .fontWeight(.bold)
+                            Spacer()
+                        }
+                        
+                        
+                        
+                        ScrollView(.horizontal) {
+                            LazyHStack(alignment: .top) {
+                                
+                                
+                            }
+                        }
+                        .frame(height: 200, alignment: .leading)
+                    }
                 }
-            }
-            .ignoresSafeArea(edges: .bottom)
-            .navigationTitle(movieTile)
-            .navigationBarTitleDisplayMode(.inline)
-            .onAppear {
-                let appearance = UINavigationBarAppearance()
-                appearance.backgroundColor = UIColor.gray.withAlphaComponent(0.2)
-                UINavigationBar.appearance().standardAppearance = appearance
-                UINavigationBar.appearance().scrollEdgeAppearance = appearance
+                .ignoresSafeArea(edges: .bottom)
+                .navigationTitle(movieTile)
+                .navigationBarTitleDisplayMode(.inline)
+                .onAppear {
+                    let appearance = UINavigationBarAppearance()
+                    appearance.backgroundColor = UIColor.gray.withAlphaComponent(0.2)
+                    UINavigationBar.appearance().standardAppearance = appearance
+                    UINavigationBar.appearance().scrollEdgeAppearance = appearance
+                }
             }
         }
     }
