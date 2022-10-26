049ae493f85bc2147471f7f30497770c173fa913
Aldair Raul Cosetito Coral <aldair.loq.369@gmail.com>
2022-10-25 20:38:42 -0500

    Added observed object which is going to calculate urls to shows on screen

--- TMDB-SwiftUI/Views/Components/CustomTopBar.swift
+++ TMDB-SwiftUI/Views/Components/CustomTopBar.swift
@@ -13,13 +13,26 @@ enum ImageCategory {
     case logo
 }
 
+typealias ImagesCategory = [ImageCategory: [ImageModel]]
+
 struct CustomTopBar: View {
+    private let baseImageUrl = "https://image.tmdb.org/t/p/w500"
     let sectionName: String
     
-    var images: [ImageModel]
+    var images: ImagesCategory
     
     var urls: [URL] {
-        return []
+        var urlArray: [URL]
+        
+        switch imageCategory {
+        case .poster:
+            urlArray = images[ImageCategory.poster]?.compactMap { URL(string: "\(baseImageUrl)\($0.filePath)") } ?? []
+        case .backdrop:
+            urlArray = images[ImageCategory.backdrop]?.compactMap { URL(string: "\(baseImageUrl)\($0.filePath)") } ?? []
+        case .logo:
+            urlArray = images[ImageCategory.logo]?.compactMap { URL(string: "\(baseImageUrl)\($0.filePath)") } ?? []
+        }
+        return urlArray
     }
     
     @State var imageCategory = ImageCategory.poster
@@ -72,31 +85,16 @@ struct CustomTopBar: View {
 struct CustomTopBar_Previews: PreviewProvider {
     static var previews: some View {
         CustomTopBar(sectionName: "Media",
-                     urls: [
-                        URL(string: "https://image.tmdb.org/t/p/w500/pfAZP7JvTTxqgq7n6A1OYgkAdEW.jpg")!,
-                        URL(string: "https://image.tmdb.org/t/p/w500/aJAZa8nNDeSFh8SxSaLzgah2u8Q.jpg")!,
-                        URL(string: "https://image.tmdb.org/t/p/w500/8kH76SeYSU32IvliZEKQ0LQeDyz.jpg")!,
-                        URL(string: "https://image.tmdb.org/t/p/w500/664alVIZgvBKqGX1rNxwykj6keJ.jpg")!,
-                        URL(string: "https://image.tmdb.org/t/p/w500/49fVsbxXTf7wpYGjMI2nTnJADb6.jpg")!,
-                        URL(string: "https://image.tmdb.org/t/p/w500/7hkN0cqtzXnQGoNPtZUYDSKR4IZ.jpg")!,
-                        URL(string: "https://image.tmdb.org/t/p/w500/rhIMDeJcYIlC0vgWFceh2B4sYuy.jpg")!,
-                        URL(string: "https://image.tmdb.org/t/p/w500/b42UaXsufDYQVjwNPaZbXVfJ38B.jpg")!,
-                        URL(string: "https://image.tmdb.org/t/p/w500/l1ORpdYr8uFZ9MNYKoq1p6fIc4M.jpg")!,
-                        URL(string: "https://image.tmdb.org/t/p/w500/e9PsdPFOAqVHqoF38ieA2QPgqR8.jpg")!,
-                        URL(string: "https://image.tmdb.org/t/p/w500/pjT1wmlDItIEX2ZHSdqg8sL6IcV.jpg")!,
-                        URL(string: "https://image.tmdb.org/t/p/w500/3yuoJwC0GGYM7Cfok9IjtkjPWrK.jpg")!,
-                        URL(string: "https://image.tmdb.org/t/p/w500/1lz4IxwC1BeO0KrjIBhXo16L8Lb.jpg")!,
-                        URL(string: "https://image.tmdb.org/t/p/w500/6x9IMtjSKlszrsowYfwvIK3xQGp.jpg")!,
-                        URL(string: "https://image.tmdb.org/t/p/w500/5KLYFQxDJN5CZYajqPF0ceBtQtc.jpg")!,
-                        URL(string: "https://image.tmdb.org/t/p/w500/xEv4jxr4IkGY9eVcQYZCNHX7O6U.jpg")!,
-                        URL(string: "https://image.tmdb.org/t/p/w500/8eLneskfRctwyy6ztDoWio5bUHE.jpg")!,
-                        URL(string: "https://image.tmdb.org/t/p/w500/aDP925IF31AZCWgTfya0EUuyXEK.jpg")!,
-                        URL(string: "https://image.tmdb.org/t/p/w500/dty8keAVIFokUzdOrlUydjPFl9V.jpg")!,
-                        URL(string: "https://image.tmdb.org/t/p/w500/hlaCx36plA50t2ULGo4WCgcTHRM.jpg")!,
-                        URL(string: "https://image.tmdb.org/t/p/w500/wqh54zv29b3x6x3ua53u4orS5lt.jpg")!,
-                        URL(string: "https://image.tmdb.org/t/p/w500/wNTD4IQWRlGLf1c6bfP0rU1i5M3.jpg")!,
-                        URL(string: "https://image.tmdb.org/t/p/w500/7MySMAZQIUjbcws9SQJxMrEpc4v.jpg")!,
-                        URL(string: "https://image.tmdb.org/t/p/w500/gBHcNqmF9H1buVbysEUge9B0tI1.jpg")!,
-                           ])
+                     images: [
+                        .backdrop: [
+                            ImageModel(aspectRatio: 1,
+                                       height: 1,
+                                       iso6391: nil,
+                                       filePath: "/7hkN0cqtzXnQGoNPtZUYDSKR4IZ.jpg",
+                                       voteAverage: 5.0,
+                                       voteCount: 1,
+                                       width: 100)
+                        ]
+                     ])
     }
 }
