b1295d77a47876e9e850acdd868c74984b14c556
Aldair Raul Cosetito Coral <aldair.loq.369@gmail.com>
2022-10-31 22:45:46 -0500

    CastNetwork created for listing cast of any media type resource

---
+++ TMDB-SwiftUI/Network/CastNetwork.swift
@@ -0,0 +1,15 @@
+//
+//  CastNetwork.swift
+//  TMDB-SwiftUI
+//
+//  Created by Aldair Cosetito Coral on 31/10/22.
+//
+
+import Foundation
+
+final class CastNetwork {
+    
+    func getCast(_ media: MediaType, id: Int) {
+        
+    }
+}
