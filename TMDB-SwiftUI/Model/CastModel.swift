2215ac7561c09f40ee0ce487b3e72295162364b0
Aldair Raul Cosetito Coral <aldair.loq.369@gmail.com>
2022-11-01 22:07:56 -0500

    Defined properties

--- TMDB-SwiftUI/Model/CastModel.swift
+++ TMDB-SwiftUI/Model/CastModel.swift
@@ -8,5 +8,31 @@
 import Foundation
 
 struct CastModel: Codable {
+    enum CodingKeys: String, CodingKey {
+        case adult = "adult"
+        case gender = "gender"
+        case id = "id"
+        case knownForDepartment = "known_for_department"
+        case name = "name"
+        case originalName = "original_name"
+        case popularity = "popularity"
+        case profilePath = "profile_path"
+        case castId = "cast_id"
+        case character = "character"
+        case creditId = "credit_id"
+        case order = "order"
+    }
     
+    let adult: Bool?
+    let gender: Int?
+    let id: Int?
+    let knownForDepartment: String?
+    let name: String?
+    let originalName: String?
+    let popularity: Double?
+    let profilePath: String?
+    let castId: Int?
+    let character: String?
+    let creditId: String?
+    let order: Int?
 }
