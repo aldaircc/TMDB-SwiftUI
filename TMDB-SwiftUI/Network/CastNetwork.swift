014fd46680ba95cd64e2c7bae43e094b5a4e7d55
Aldair Raul Cosetito Coral <aldair.loq.369@gmail.com>
2022-11-02 18:54:57 -0500

    Added method for consuming aPI

--- TMDB-SwiftUI/Network/CastNetwork.swift
+++ TMDB-SwiftUI/Network/CastNetwork.swift
@@ -9,7 +9,25 @@ import Foundation
 
 final class CastNetwork {
     
-    func getCast(_ media: MediaType, id: Int) {
+    func getCast(_ media: MediaType, id: Int, completion: @escaping (Result<Casts, Error>) -> ()) {
+        guard let url = URL(string: "https://api.themoviedb.org/3/\(media.rawValue)/\(id)/credits?api_key=") else {
+            return
+        }
         
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data,
+                  let httpResponse = response as? HTTPURLResponse,
+                  error == nil else {
+                return
+            }
+            
+            if httpResponse.statusCode == 200 {
+                if let response = try? JSONDecoder().decode(Casts.self, from: data) {
+                    completion(.success(response))
+                }
+            } else {
+                
+            }
+        }
     }
 }
