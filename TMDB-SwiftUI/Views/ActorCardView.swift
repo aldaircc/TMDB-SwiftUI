af215368ff65768e25c9be0e5002d48f3e098a01
Aldair Raul Cosetito Coral <aldair.loq.369@gmail.com>
2022-11-03 22:39:24 -0500

    Removed comments

--- TMDB-SwiftUI/Views/ActorCardView.swift
+++ TMDB-SwiftUI/Views/ActorCardView.swift
@@ -12,42 +12,6 @@ struct ActorCardView: View {
     let character: String
     
     var body: some View {
-//        ZStack(alignment: .bottomLeading) {
-//            Image(name)
-//                .resizable()
-//                .scaledToFit()
-//                .frame(width: 117, height: 175, alignment: .center)
-//                .background(.yellow)
-//                .cornerRadius(10)
-//
-//            ZStack(alignment: .leading) {
-//                Rectangle()
-//                    .fill(.white)
-//                    .frame(width: 117, height: 35)
-//                VStack(alignment: .leading) {
-//                    HStack {
-//                        Text(name)
-//                            .font(.system(size: 12))
-//                            .fontWeight(.semibold)
-//                    }
-//                    .background(Color.white)
-//                    HStack {
-//                        Text(character)
-//                            .font(.system(size: 10))
-//                            .fontWeight(.light)
-//
-//                    }
-//                }
-//                .padding(.horizontal, 5)
-//            }
-//            .cornerRadius(1)
-//        }
-//        .overlay(
-//            RoundedRectangle(cornerRadius: 9, style: .continuous)
-//                .stroke(.black.opacity(0.1), lineWidth: 0.4)
-//                .shadow(color: .black, radius: 10, x: 0, y: 0)
-//        )
-        
         ZStack(alignment: .bottomLeading) {
             Image("tom_cruise")
                 .resizable()
