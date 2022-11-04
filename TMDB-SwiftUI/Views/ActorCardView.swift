194a8f0461c2d82dc0d8d92294f1f9b184425618
Aldair Raul Cosetito Coral <aldair.loq.369@gmail.com>
2022-11-03 22:55:09 -0500

    Added extension and methods to draw corner on each side of any view

--- TMDB-SwiftUI/Views/ActorCardView.swift
+++ TMDB-SwiftUI/Views/ActorCardView.swift
@@ -57,3 +57,19 @@ struct ActorInformationBox_Previews: PreviewProvider {
         ActorInformationBox(actorName: "Aldair Cosetito", character: "iOS Engineer")
     }
 }
+
+extension View {
+    func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
+        Text("xx")
+    }
+}
+
+struct RoundedCorner: Shape {
+    var radius: CGFloat = .infinity
+    var corners: UIRectCorner = .allCorners
+    
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        return Path(path.cgPath)
+    }
+}
