0d3917be3d905e2ba777c91d418638490592a4e1
Aldair Cosetito Coral <aldair.loq.369@gmail.com>
2022-11-07 23:13:40 -0500

    Added method to get casts

--- TMDB-SwiftUI/Network/MovieNetwork.swift
+++ TMDB-SwiftUI/Network/MovieNetwork.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+struct MovieNetwork {
+    
+    func getCast(_ media: MediaType, id: Int, completion: @escaping (Result<Casts, Error>) -> ()) {
+        guard let url = URL(string: "https://api.themoviedb.org/3/\(media.rawValue)/\(id)/credits?api_key=") else {
+            return
+        }
+        
+        Network().callApi(url: url, object: Casts.self) { result in
+            switch result {
+            case .success(let data):
+                completion(.success(data))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+}
