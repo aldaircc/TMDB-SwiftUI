36eba2a00fca779fa007de56be5f2c804fdde6e5
Aldair Cosetito Coral <aldair.loq.369@gmail.com>
2022-11-24 22:44:52 -0500

    Added column for view and character fields

---
+++ TMDB-SwiftUI/Views/Cast/CastView.swift
@@ -0,0 +1,27 @@
+//
+//  CastView.swift
+//  TMDB-SwiftUI
+//
+//  Created by Aldair Cosetito Coral on 23/11/22.
+//
+
+import SwiftUI
+
+struct CastView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                VStack {
+                    Text("Name")
+                    Text("Character")
+                }
+            }
+        }
+    }
+}
+
+struct CastView_Previews: PreviewProvider {
+    static var previews: some View {
+        CastView()
+    }
+}
