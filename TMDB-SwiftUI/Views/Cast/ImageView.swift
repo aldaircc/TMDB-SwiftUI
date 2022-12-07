7b409033eea35371e6749949051a205458db207e
Aldair Cosetito Coral <aldair.loq.369@gmail.com>
2022-12-06 22:55:27 -0500

    Added enum for evaluating and applying modifier

--- TMDB-SwiftUI/Views/Cast/ImageView.swift
+++ TMDB-SwiftUI/Views/Cast/ImageView.swift
@@ -7,23 +7,39 @@
 
 import SwiftUI
 
+enum PictureMode {
+    case photo
+    case landScape
+    case none
+}
+
 struct ImageView: View {
     @ObservedObject var imageLoader: ImageLoader
     @State var image = UIImage()
     let defaultImage = UIImage(named: "none-image")!
+    let pictureMode: PictureMode
     
-    init(withURL url: URL) {
+    init(withURL url: URL, pictureMode: PictureMode = .photo) {
         imageLoader = ImageLoader(urlString: url)
+        self.pictureMode = pictureMode
     }
     
-    var body: some View {
+    var pictureView: some View {
         Image(uiImage: image)
             .resizable()
             .aspectRatio(contentMode: .fit)
-            .frame(height: (image != defaultImage) ? 100 : 65, alignment: .center)
-            .cornerRadius(10)
             .onReceive(imageLoader.didChange) { data in
                 self.image = UIImage(data: data) ?? defaultImage
             }
+    }
+    
+    var body: some View {
+        switch pictureMode {
+        case .photo:
+            pictureView
+                .frame(height: (image != defaultImage) ? 100 : 65, alignment: .center)
+        case .landScape, .none:
+            pictureView
+        }
     }
 }
